23c7221c61cd42dec96f23d8db336f71e8106177
Jean Luc SEMEDO <francisco.luc93@gmail.com>
2019-02-03 22:17:51 +0000

    Before migrations

--- vote_app/settings.py
+++ vote_app/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'votes',
         'USER' : 'root',
-        'PASSWORD' : 'patosh93',
+        'PASSWORD' : '',
         'HOST' : 'localhost',
         'PORT' : '3306'
     }
